186cf83bd8bbf6f5dfbea9c6fa3a5b782875ced2
valentin.hassler <valentin.hassler@stud.uni-goettingen.de>
2021-05-13 19:26:03 +0200

    Model_Matrix wird erstellt

--- R/Model_Matrix/Model_Matrix.R
+++ R/Model_Matrix/Model_Matrix.R
@@ -1,7 +1,7 @@
 ####Einstieg konstruieren eines Splines der Ordnung 1#####
 set.seed(100)
-x <- runif(1000,0,20)
-y <- sin(x) +rnorm(1000)
+x <- runif(100,0,20)
+y <- sin(x) +rnorm(100)
 plot(x,y)
 
 Knoten <- seq(1,10, length.out = 10)
@@ -37,24 +37,26 @@ test <-function(){
 
 # Definition des Grundsplines, auf dem die anderen aufbauen
 B_0 <- function(z,kj1,kj2){
-  if (kj1 < z & z < kj2 ){
+  if (kj1 <= z & z <= kj2 ){
+
     return(1)
   }else{return(0)}
 }
 
 
-#Hier wird jetzt die Splinesfunktion definiert
+#Hier wird jetzt die Grundfunktion für die Knotenpunkte definiert
 
 # range     In welchem Bereich soll definiert werden (Vektor mit 2 Elementen)
 # kn        Welche Knotenzahl ist gewünscht
 # order     Welche order soll der
 
-knotposition <- function(range, kn, order){
+splinmitte <- function(range, kn, order)
+{
   #distanz zwischen zwei Punkten
-  distance <- function(object){
+  distance <- function(object)
+  {
     return(object[2] - object[1])
   }
-
   #abstand zwischen 2 Punkten
   onedist <- distance(range) / (kn - 1)
   return(seq(from = (range[1] - onedist * order), to = (range[2] + onedist * order),
@@ -68,8 +70,10 @@ knotposition <- function(range, kn, order){
 #z      der Wert an der Stelle an der ausgewertet wird
 
 
-splin <- function(kp, j, z, order){
-  if (order == 0){
+splin <- function(kp, j, z, order)
+{
+  if (order == 0)
+  {
     return(B_0(z, kp[j], kp[j + 1]))
   }
   k <- ((z - kp[j - order])/(kp[j] - kp[j - order])) * splin(kp, j-1 , z, order - 1) +
@@ -77,9 +81,11 @@ splin <- function(kp, j, z, order){
   return(k)
 }
 
-splines <- function(z, range, kn, order, j){
-  kp <- knotposition(range, kn, order)
-  #damit die richtige Knotenposition angegeben wird
+
+splines <- function(z, range, kn, order, j)
+{
+  kp <- splinmitte(range, kn, order)
+  #SPlinmittelpunkt
   j <- j + order
   splin(kp, j, z, order)
 }
@@ -87,18 +93,62 @@ splines <- function(z, range, kn, order, j){
 
 # wie wird es dann grafisch dargestellt: Eigentlich erst später durchführbar, aber hier schonmal Gerüst
 
-splines(0.001,range = c(0,10), kn = 2,order = 5,j = 1)
+splines(0.1,range = c(0,10), kn = 11,order = 1,j = 1)
 
-p <- ggplot(data = data.frame(x = c(1, 10)), aes(x))# + ylim(c(0,3))
+p <- ggplot(data = data.frame(x = c(0, 10)), aes(x))# + ylim(c(0,3))
 test <-function(){
   p + stat_function(fun = Wrapper,n = 1000, geom = "area", fill = "blue", alpha = 0.5)
 }
 
 Wrapper <- function(z){
-  sapply(z, splines, range = c(0,10), kn = 11,order = 1,j = 3)
+  sapply(z, splines, range = c(0,10), kn = 11,order = 2,j = 4)
 }
 test()
 
+z <- c(2.4,3.2,4.4)
+kn <- 11
+model <- matrix(0, nrow = length(z), ncol = kn - 1)
+for ( i in 1:length(z)){
+  for( j in 1:(kn - 1)){
+    model[i,j] <- splines( z = z[i] ,range = c(0,10), kn = 11,order = 5, j = j )
+  }
+}
+
+####Model_Matrix####
+#jetzt wird die Modell Matrix (Z) definiert
+#z    Werte der Kovariaten
+#kn   Gewünschte Knotenzahl
+model_matrix <- function(z, kn, range, order){
+  model <- matrix(0, nrow = length(z), ncol = kn - 1)
+  for ( i in 1:length(z))
+  {
+    for( j in 1:(kn - 1))
+    {
+      model[i,j] <- splines( z = z[i] ,range = c(0,10), kn = 11,order = 2, j = j )
+    }
+  }
+  return(model)
+}
+Z <- model_matrix(z = x, kn = 11,range = c(0,10), order = 3)
+koeffizienten <- crossprod(Z) %*% t(Z) %*% y
+koeffizienten
+
+####Zusammensetzen der FUnktion funktioniert nohc nicht####
+# jetzt soll die Funktion zusammengefügt werden
+
+ts <- numeric(20)
+for (p in seq(0,10, length.out = 20)) {
+  summe <- 0
+  #bildet die Summe der einzelnen
+  for ( j in 1:10){
+    summe <- summe + koeffizienten[j]* splines( z = p,range = c(0,10), kn = 11,order = 2, j = j)
+  }
+  ts[p] <- summe
+}
+
+
+ts
+#splines(z = 0.1,range = c(0,10), kn = 11,order = 1,j = 1)
 
 
 
