79e32cd3fc60261f407ded3f83f386bf3df151ef
jan.schneider01 <jan.schneider01@stud.uni-goettingen.de>
2021-05-24 20:17:51 +0200

    minor corrections and clean up of bspline Link file
    added wrapper function for convenience

--- R/Model_Matrix/bspline Link.R
+++ R/Model_Matrix/bspline Link.R
@@ -49,67 +49,64 @@ bspline = function(m, knots, d, lslm = F)
 
 }
 
-# Thu May 20 10:21:52 2021 ------------------------------
-
-# Random data
-require(lslm)
-set.seed(1)
-n = 1000
-xa = seq(0,1, length.out = n)
-y = xa + rnorm(n, 0, 0.3)
-m = lslm(y ~ xa, light = F)
 
 
-# trying to implement it with lslm not working as of now
-#
-# x = mspline$x
-# mspline = lslm(y ~ x[,1] + x[,2]+ x[,3]+ x[,4]+ x[,5] - 1, light = F)
-# summary(mspline)
-#
-#
-# plot(xa, y)
-# points(predict(mspline)$location, y, col = "green")
+
+# Mon May 24 19:30:25 2021 ------------------------------
+# optimizing penalty matrix
+
+penalty = function(knots,diffd = 2)
+{
+  if (diffd == 1)
+  {
+    ones = cbind(0,diag(knots - 1))
+    nones = cbind(diag(knots - 1) * -1, 0)
+    K = ones + nones
+    K = t(K) %*% K
+    return(ones + nones)
+  }
+
+  # recursive definition of penalty matrix
+  #shrink starting knots such that end knots are correct
+  knots = knots - diffd + 1
+  K = penalty(knots, 1)
+  for (i in 1:(diffd - 1))
+  {
+    K = K %*% penalty(knots + i, 1)
+  }
+  K = t(K) %*% K
+  return(K)
+}
 
 
-# Thu May 20 10:22:44 2021 ------------------------------
-# implementing a ML estimator
-
-x = bspline(m, 40, 3)
-
-beta = solve(t(x) %*% x) %*% t(x) %*% y
-pred = x %*% beta
+penalty(9,2)
 
 
-plot(xa, y)
-points(xa, pred, col = 2)
-lines(xa, xa, col = 3)
 
 
-# testing performance for difficult data
+# wrapper
+ml = function(xa,y, kn, d, diffd, lambda)
+{
+  m = lslm(y ~ xa, light = F)
+  x = bspline(m, knots = kn, d = d)
+  K = penalty(knots = kn + d - 1, diffd = diffd)
+
+  beta = solve(t(x) %*% x + lambda * K) %*% t(x) %*% y
+  pred = x %*% beta
 
 
-n = 1000
-xa = runif(n,0,10)
-xa = sort(xa)
-s = (exp(0.2*xa)-1)/10+0.3
-epsilon = rnorm(n,0, s)
+  plot(xa, y)
+  lines(xa, pred, col = 2, lwd = 5)
 
-ytrue =sin(xa*4) *xa/4
-plot(xa, ytrue)
-y = ytrue + epsilon
-plot(xa,y)
+}
 
 
-m = lslm(y ~ xa, light = F)
-x = bspline(m, 200, 3)
+x = seq(0,10,length.out = 100)
+y = 0.1 * x + rnorm(100,0,0.3)
+plot(x,y)
+ml(x,y,kn = 20, d = 4, diffd = 1, lambda = 0.1)
 
-beta = solve(t(x) %*% x) %*% t(x) %*% y
-pred = x %*% beta
 
-
-plot(xa, y)
-lines(xa, pred, col = 2, pch = 5)
-points(xa, ytrue, col = 3)
 
 
 
