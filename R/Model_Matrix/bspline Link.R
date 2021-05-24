63f48313f00f440adca18cf4bb721ac50a1324b5
jan.schneider01 <jan.schneider01@stud.uni-goettingen.de>
2021-05-24 19:27:24 +0200

    clean up and test

--- R/Model_Matrix/bspline Link.R
+++ R/Model_Matrix/bspline Link.R
@@ -1,10 +1,12 @@
 "   VARIABLES
       Input
+m       - lslm object
 knots   - positive integer      Number of knots
 i       - positive integer      index of knots
 d       - positive integer      degree of spline
 range   - tulpel of two floats  range of spline
 x       - vector of floats      input value for bspline function
+lslm    - Modus if an LSLM object is returned or just the model matrix
 
       Other
 pos   - vector of length knots + 2 * d
@@ -19,7 +21,7 @@ source("R/Model_Matrix/bsplinesJan.R")
 
 
 # returns a suitable modelmatrix for bspline
-bspline = function(m, knots, d)
+bspline = function(m, knots, d, lslm = F)
 {
   # removing superfluous intercept
   if (all(m$x[,1] == 1))
@@ -39,25 +41,75 @@ bspline = function(m, knots, d)
     mat[,i]= basis(knots, i, d, range, m$x, pos = NA)
   }
   m$x = model.matrix(~ . - 1, data = mat)
+  if (lslm == F)
+  {
+    return(m$x)
+  }
   return(m)
 
 }
 
+# Thu May 20 10:21:52 2021 ------------------------------
 
-
-install.packages("lslm")
+# Random data
 require(lslm)
-xa = seq(0,1, length.out = 100)
-y = xa + rnorm(100)
+set.seed(1)
+n = 1000
+xa = seq(0,1, length.out = n)
+y = xa + rnorm(n, 0, 0.3)
 m = lslm(y ~ xa, light = F)
 
-x = mspline$x
-mspline = lslm(y ~ x[,1] + x[,2]+ x[,3]+ x[,4]+ x[,5] - 1, light = F)
-summary(mspline)
+
+# trying to implement it with lslm not working as of now
+#
+# x = mspline$x
+# mspline = lslm(y ~ x[,1] + x[,2]+ x[,3]+ x[,4]+ x[,5] - 1, light = F)
+# summary(mspline)
+#
+#
+# plot(xa, y)
+# points(predict(mspline)$location, y, col = "green")
+
+
+# Thu May 20 10:22:44 2021 ------------------------------
+# implementing a ML estimator
+
+x = bspline(m, 40, 3)
+
+beta = solve(t(x) %*% x) %*% t(x) %*% y
+pred = x %*% beta
 
 
 plot(xa, y)
-points(predict(mspline)$location, y, col = "green")
+points(xa, pred, col = 2)
+lines(xa, xa, col = 3)
+
+
+# testing performance for difficult data
+
+
+n = 1000
+xa = runif(n,0,10)
+xa = sort(xa)
+s = (exp(0.2*xa)-1)/10+0.3
+epsilon = rnorm(n,0, s)
+
+ytrue =sin(xa*4) *xa/4
+plot(xa, ytrue)
+y = ytrue + epsilon
+plot(xa,y)
+
+
+m = lslm(y ~ xa, light = F)
+x = bspline(m, 200, 3)
+
+beta = solve(t(x) %*% x) %*% t(x) %*% y
+pred = x %*% beta
+
+
+plot(xa, y)
+lines(xa, pred, col = 2, pch = 5)
+points(xa, ytrue, col = 3)
 
 
 
