b07eb57bdedd2febb09ded8b57f451491f508e02
valentin.hassler <valentin.hassler@stud.uni-goettingen.de>
2021-05-26 00:01:32 +0200

    erstellen einer einfacen Print funktion

---
+++ R/spline/print_function.R
@@ -0,0 +1,14 @@
+
+
+print.spl <- function(m, sd = 1.96)
+{
+  require(ggplot2)
+  ggplot(mapping = aes(x = x)) +
+    geom_point(aes(y = y)) +
+    geom_line(aes(y = m$spline[["fitted.values"]]$location), colour = "green", size = 2)+
+    geom_line(aes(y = m$spline[["fitted.values"]]$location + sd * m$spline[["fitted.values"]]$scale ), colour = "blue", size = 1)+
+    geom_line(aes(y = m$spline[["fitted.values"]]$location - sd * m$spline[["fitted.values"]]$scale ), colour = "blue", size = 1)+
+    ylab("dependent variable")+
+    xlab("explaining variable")
+}
+
