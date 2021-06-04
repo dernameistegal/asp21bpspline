64e1f644713c0fd3544aad2e90a2d7e696d153c6
jan.schneider01 <jan.schneider01@stud.uni-goettingen.de>
2021-06-04 19:56:21 +0200

    Introduced Class system for function choice in iteration loop. Added skeleton functions for burning, thinning, sampling, and setting starting values

--- R/mcmc_framework_jan.R
+++ R/mcmc_framework_jan.R
@@ -11,19 +11,97 @@ thinninginteger   no of iterations to be ignored between two consecutive iterati
 
 mcmc = function(m, it, burning, thinning)
 {
-  i = 0
+
   sample_tau = numeric(it)
   sample_epsilon = numeric(it)
-  sample_beta = matrix(NA, ncol = it, nrow = extknots)
-  sample_gamma = matrix(NA, ncol = it, nrow = extknots)
+  sample_beta = matrix(NA, ncol = it, nrow = 10) # change 10 to number of intervals
+  sample_gamma = matrix(NA, ncol = it, nrow = 10) # change 10 to number of intervals
 
-  sample_list = list(sample_tau, sample_epsilon, sample_beta, sample_gamma)
+  sample_list = list(tau = sample_tau,
+                     beta = sample_beta,
+                     epsilon = sample_epsilon,
+                     gamma = sample_gamma)
+
+  sample_list = set_start_values(sample_list, m)
 
   for (i in 1:it)
   {
-    sample_tau[i] = sample(choose_previous_values(sample_list), method = "tau")
-    sample_beta[i] = sample(choose_previous_values(sample_list), method = "beta")
-    sample_epsilon[i] = sample(choose_previous_values(sample_list), method = "epsilon")
-    sample_gamma[i] = sample(choose_previous_values(sample_list), method = "gamma")
+    sample_list[[1]][i] = sample(sample_list, method = "tau")
+    sample_list[[2]][i,] = sample(sample_list, method = "beta")
+    sample_list[[3]][i] = sample(sample_list, method = "epsilon")
+    sample_list[[4]][i, ] = sample(sample_list, method = "gamma")
   }
+
+  sample_list = burn(sample_list, burning)
+  sample_list = thin(sample_list, thinning)
+
+  return(sample_list)
 }
+
+
+sample = function(sample_list, method)
+{
+  class(sample_list) = method
+  UseMethod("sample", sample_list)
+}
+
+
+sample.tau = function(sample_list, method)
+{
+  #todo
+
+  return(NA)
+}
+
+
+sample.beta = function(sample_list, method)
+{
+  #todo
+
+  return(rep(NA, times = ncol(sample_list$beta)))
+}
+
+
+sample.epsilon = function(sample_list, method)
+{
+  #todo
+
+  return(NA)
+}
+
+
+sample.gamma = function(sample_list, method)
+{
+  #todo
+
+  return(rep(NA, times = ncol(sample_list$beta)))
+}
+
+
+
+set_start_values = function(sample_list, m)
+{
+  #todo
+  return(sample_list)
+}
+
+
+burn = function(sample_list, burning)
+{
+  #todo
+  return(sample_list)
+}
+
+
+thin = function(sample_list, thinning)
+{
+  #todo
+  return(sample_list)
+}
+
+mcmc(2, 10, 10, 10)
+
+
+
+
+
