97c629c553f1fb1547c0e9a836695d594a0e4020
jonathan.henrich <jonathan.henrich@stud.uni-goettingen.de>
2021-06-06 02:34:30 +0200

    added penalization for gamma

--- R/spline/spline.R
+++ R/spline/spline.R
@@ -16,7 +16,7 @@ y =  x + rnorm(500, 0, 1) * (0.5 + x*0.3)
 m = lslm(y ~ x, light = F)
 #penalization problem etwas verbessert, nun haben große lambda eine große Wirkung, aber wir müssen noch überlgen, ob update gamma auch betroffen ist
 #bis jetzt ist nur update beta etwas verändert
-m = spline_user_function(m = m, kn =  11, order =  3, p_order =  3,lambda =  1)
+m = spline_user_function(m = m, kn =  11, order =  3, p_order =  1,lambda =  1)
 #so ist das Modell mit splines
 print.spl(m, sd = 1.96)
 
