42ffbfd57af7dff4559f0916e97bc1af5bad75df
Valentin Hassler <Duplexmayer@gmail.com>
2021-07-29 23:19:26 +0200

    some idea for plotting. Simulation 2, just copied simulation 1 right now

--- simulation/simulation 2/do simulation 2.r
+++ simulation/simulation 2/do simulation 2.r
@@ -6,8 +6,9 @@ beta = read.csv("simulation/simulation 1/beta_sim1")
 gamma = read.csv("simulation/simulation 1/gamma_sim1")
 
 simulation1 =  varlist(
-  n.sim = list(type = "N", expr = quote(N[sim]), value = 20),
+  n.sim = list(type = "N", expr = quote(N[sim]), value = 10),
   n = list(type = "grid", value = 1000),
+  it = list(type = "frozen", value = 1000),
   beta = list(type = "frozen", expr = quote(beta), value = beta),
   gamma = list(type = "frozen", expr = quote(gamma), value = gamma),
   knots = list(type = "frozen", value = c(15, 15)),
@@ -16,7 +17,7 @@ simulation1 =  varlist(
   smooth =  list(type = "frozen", value = c(0,0)))
 
 
-doOne = function(n, beta, gamma, knots, order, p_order, smooth)
+doOne = function(n,it, beta, gamma, knots, order, p_order, smooth)
 { 
   # generate sample
   x = runif(n, 0, 10)
@@ -43,14 +44,19 @@ doOne = function(n, beta, gamma, knots, order, p_order, smooth)
   model = spline(m1, knots, order = order, p_order = p_order, smooth = smooth)
   loc = model$coefficients$location 
   scale = model$coefficients$scale 
-  return(cbind(loc, scale))
+  
+  
+  model = mcmc.spline(model, it = it, burning = 1, thinning = 1)
+  locmcmc = colMeans(model$beta)
+  scalemcmc = colMeans(model$gamma)
+  return(cbind(loc, scale, locmcmc, scalemcmc))
 }
 
-doOne(n = 1000, beta, gamma, knots = c(15, 15), order = c(3, 3), 
+doOne(n = 1000, it = 1000, beta, gamma, knots = c(15, 15), order = c(3, 3), 
       p_order = c(0,0), smooth = c(0,0))
 
 a = Sys.time()
-res10 = doLapply(simulation1, sfile = "simulation/simulation 1/take_1", doOne = doOne, monitor = T)
+res10 = doLapply(simulation1, sfile = "simulation/simulation 1/take_2", doOne = doOne)
 b = Sys.time()
 b-a
 
