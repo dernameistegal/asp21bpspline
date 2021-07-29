d88caa5f75e55c88f47e59ee6fdbb6e15e7f0114
Valentin Hassler <Duplexmayer@gmail.com>
2021-07-29 16:26:03 +0200

    minimal example extended

--- simulation/minimal example simsalapar.R
+++ simulation/minimal example simsalapar.R
@@ -10,7 +10,6 @@ simulation1 =  varlist(
 toLatex(simulation1)
 mkGrid(simulation1)
 
-
 doOne = function(n, nvars)
 {
   x = matrix(0, nrow = n, ncol = nvars)
@@ -22,5 +21,16 @@ doOne = function(n, nvars)
   R = sum(lm(y~x)$coef)
   return(R)
 }
-doOne(10, 1)
+#doOne(10, 1)
 res = doLapply(simulation1, sfile = "simulation/simtest", doOne = doOne)
+array2df(getArray(res))
+str(getArray(res))
+val <- getArray(res)
+err <- getArray(res, "error")
+warn <- getArray(res, "warning")
+time <- getArray(res, "time")
+
+rv <- c( "nvars")
+cv <- c( "n")
+ftable(100 * (err + warn), row.vars = rv, col.vars = cv)
+ftable(time, row.vars = rv, col.vars = cv)
