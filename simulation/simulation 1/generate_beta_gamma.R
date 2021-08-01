2faf3f8bc2c2e1d685b92bca2c2fa4a6df76da8c
Jonathan <jonathan.henrich@web.de>
2021-08-01 17:25:54 +0200

    wiggly estimate of function to use for simulation study

--- simulation/simulation 1/generate_beta_gamma.R
+++ simulation/simulation 1/generate_beta_gamma.R
@@ -3,13 +3,13 @@
 require(lmls)
 require(asp21bpspline)
 
-set.seed(1)
-n = 1000
-x = seq(0,20, length.out = n)
+set.seed(2)
+n = 100
+x = c(runif(100, 0, 20))
 mean = -0.04*x^2 + 0.5* x 
-y =  mean + rnorm(n, 0, 0.05 *x + 0.3)
+y =  mean + rnorm(n, 0, 0.2 *x + 0.3)
 m1 = lmls(y~x, scale = ~x, light = F)
-m = spline(m1, c(15,15), order = c(3,3), p_order = c(2,2), smooth = c(1,1))
+m = spline(m1, c(15,15), order = c(3,3), p_order = c(0,0), smooth = c(0,0))
 
 plot(m, sd = 1.96)
 
