91cab35bbbc93b77d5e5758ae90e36a3b4679001
Valentin Hassler <Duplexmayer@gmail.com>
2021-08-05 15:43:29 +0200

    renamed auswertung simulation 3

--- simulation/simulation 3/simulation3_analysis.R
+++ simulation/simulation 3/simulation3_analysis.R
@@ -1,28 +1,54 @@
-source("simulation/simulation functions_mainfolder.R")
-source("simulation/analysis_functions.R")
+
 library(simsalapar)
+simulation3 =  varlist(
+  n.sim = list(type = "N", expr = quote(N[sim]), value = 4),
+  init = list(type = "grid", value = c("normal", "random" ,"error")),
+  n = list(type = "frozen", value = 1000),
+  it = list(type = "frozen", value = 1500),
+  beta = list(type = "frozen", expr = quote(beta), value = beta),
+  gamma = list(type = "frozen", expr = quote(gamma), value = gamma),
+  knots = list(type = "frozen", value = c(15, 15)),
+  order = list(type = "frozen", value = c(3, 3)),
+  p_order = list(type = "frozen", value = c(0,0)),
+  smooth =  list(type = "frozen", value = c(0,0)),
+  burning =  list(type = "frozen", value = 500),
+  thinning =  list(type = "frozen", value = 10))
+source("simulation/general functions/analysis_functions.R")
+source("simulation/general functions/generic_simulation_functions.R")
+source("simulation/simulation 3/simulation3_functions.R")
 res3 = maybeRead("simulation/simulation 3/simulation3_test1")
-a = res3[[1]]$value
 
 
-normal_res = all_ToNormal(res3[1,])
-findmea
+library(ggplot2)
 
-x = seq(0,20, length.out = 100)
-findmean(res3[1,])
-estimations = getEstimateValues(res3[[1]]$value,simulation3, x)
-dim(getQuantiles(estimations, quantile = c(0.8,0.7,0.9)))
+x <- seq(0,20, length.out = 100)
 
-est_normal = estimate_quantile_splines(res3[1,], x, quantile = c(0.1,0.9),simulation = simulation3)
-est_random = estimate_quantile_splines(res3[2,], x, quantile = c(0.1,0.9),simulation = simulation3)
-est_error = estimate_quantile_splines(res3[3,], x, quantile = c(0.1,0.9),simulation = simulation3)
+#remove stuck mcmcs
+clean_normal = CleanStuck(res3[1,])
+clean_random = CleanStuck(res3[2,])
+clean_error = CleanStuck(res3[3,])
 
-plot(x,est_normal[1,,1], type = "l")
-lines(x,est_normal[2,,1], type = "l")
-lines(x,est_random[1,,1], type = "l", col = 2)
-lines(x,est_random[2,,1], type = "l", col = 2)
-lines(x,est_error[1,,1], type = "l", col = 3)
-lines(x,est_error[2,,1], type = "l", col = 3)
+#object gets transformed to normal simualtion objects like in simulation 2 and 1
+res_normal = all_ToNormal(resline = clean_normal)
+res_random = all_ToNormal(resline = clean_random)
+res_error = all_ToNormal(resline = clean_error)
 
+# estimations for the mean of Scale and location for the mcmcs are made
+est_mean_normal = predict_simulation(findmean(res_normal, 3),findmean(res_normal,4),simulation3,x)
+est_mean_random = predict_simulation(findmean(res_random, 3),findmean(res_random,4),simulation3,x)
+est_mean_error = predict_simulation(findmean(res_error, 3),findmean(res_error,4),simulation3,x)
 
+#estimations for the pointwise quantiles for mcmc are made 
+est_quantile_normal = estimate_quantile_splines(clean_normal, x, quantile = c(0.1,0.9),simulation = simulation3)
+est_quantile_random = estimate_quantile_splines(clean_random, x, quantile = c(0.1,0.9),simulation = simulation3)
+est_quantile_error = estimate_quantile_splines(clean_error, x, quantile = c(0.1,0.9),simulation = simulation3)
 
+#the functions are plotted
+plot_simulation3(est_mean_normal, est_quantile_normal,x)
+plot_simulation3(est_mean_random, est_quantile_random,x)
+plot_simulation3(est_mean_error, est_quantile_error,x)
+#   
+# a = predict_simulation(beta, gamma,simulation3, x )
+# ggplot(mapping = aes(x = x)) + geom_line(aes(y = a[[1]])) + geom_line(aes(y = a[[1]]+ 1.96*a[[2]]))+
+#    geom_line(aes(y = a[[1]]- 1.96*a[[2]]))
+# 
